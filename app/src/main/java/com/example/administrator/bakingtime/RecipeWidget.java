f322ed8cfdf78856db78107492b1d671af325c1f
Muhammad Alkaff <m.alkaff9@gmail.com>
2017-08-21 11:44:46 +0800

    need widget listview

--- app/src/main/java/com/example/administrator/bakingtime/RecipeWidget.java
+++ app/src/main/java/com/example/administrator/bakingtime/RecipeWidget.java
@@ -3,6 +3,7 @@ package com.example.administrator.bakingtime;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
@@ -19,13 +20,14 @@ import java.util.List;
 public class RecipeWidget extends AppWidgetProvider {
     List<Recipe> recipeList;
 
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager, List<Recipe> recipeList,
-                                int appWidgetId) {
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId, List<Recipe> recipeList) {
 
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.recipe_widget);
 
         Intent intent = new Intent(context, MainActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+        views.setTextViewText(R.id.appwidget_text, recipeList.get(0).getName());
         views.setOnClickPendingIntent(R.id.appwidget_text, pendingIntent);
 
         // Instruct the widget manager to update the widget
@@ -35,11 +37,6 @@ public class RecipeWidget extends AppWidgetProvider {
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
 
-
-        // There may be multiple widgets active, so update all of them
-        for (int appWidgetId : appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, recipeList, appWidgetId);
-        }
     }
 
     @Override
@@ -55,7 +52,17 @@ public class RecipeWidget extends AppWidgetProvider {
     @Override
     public void onReceive(Context context, Intent intent) {
         recipeList = intent.getParcelableArrayListExtra("recipes");
-        Log.d("coba", recipeList.get(0).getName());
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context.getApplicationContext());
+        ComponentName recipeWidget = new ComponentName(context.getApplicationContext(), RecipeWidget.class);
+        int[] appWidgetIds = appWidgetManager.getAppWidgetIds(recipeWidget);
+        updateRecipeWidgets(context, appWidgetManager, appWidgetIds, recipeList);
+    }
+
+    public static void updateRecipeWidgets(Context context, AppWidgetManager appWidgetManager,
+                                           int[] appWidgetIds, List<Recipe> recipeList) {
+        for (int appWidgetId: appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId, recipeList);
+        }
     }
 }
 
