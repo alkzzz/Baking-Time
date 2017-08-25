d0b18556355fe43782e6590c029ed6873c792377
Muhammad Alkaff <m.alkaff9@gmail.com>
2017-08-25 09:36:33 +0800

    fixing for review

--- app/src/androidTest/java/com/example/administrator/bakingtime/MainActivityTest.java
+++ app/src/androidTest/java/com/example/administrator/bakingtime/MainActivityTest.java
@@ -1,18 +1,31 @@
 package com.example.administrator.bakingtime;
 
+import android.support.test.espresso.ViewAction;
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.espresso.intent.Intents;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.view.View;
 
+import com.example.administrator.bakingtime.ui.DetailActivity;
 import com.example.administrator.bakingtime.ui.MainActivity;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasExtra;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasExtraWithKey;
+import static android.support.test.espresso.matcher.ViewMatchers.isCompletelyDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 @RunWith(AndroidJUnit4.class)
@@ -21,9 +34,28 @@ public class MainActivityTest {
     @Rule
     public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
 
-    @Test
-    public void checkRecipeIsDisplayed() {
-        onView(withText("Nutella Pie")).check(matches(isDisplayed()));
+    @Before
+    public void setup() {
+        Intents.init();
     }
 
+    @Test
+    public void onLoad_activityDisplaysRecyclerViewOfRecipes() {
+        onView(withId(R.id.rv_recipe)).check(matches(isCompletelyDisplayed()));
+    }
+
+    @Test
+    public void onClickingRecipeCard_OpenRecipeDetails() {
+        onView(withId(R.id.rv_recipe))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+        intended(hasComponent(DetailActivity.class.getName()));
+        intended(hasExtra("recipe_id", 1));
+    }
+
+    @After
+    public void tearDown() {
+        Intents.release();
+    }
 }
+
+
